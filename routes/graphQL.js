1eb5534e4c7d53089ad48554668013710d450802
CWD <cwd@cloudspokes.com>
2016-09-16 08:36:42 -0400

    first commit!

---
+++ routes/graphQL.js
@@ -0,0 +1,28 @@
+const graphqlHTTP = require('express-graphql');
+
+const graphql = require('graphql');
+const GraphQLSchema = graphql.GraphQLSchema;
+const GraphQLObjectType = graphql.GraphQLObjectType;
+const GraphQLString = graphql.GraphQLString;
+
+
+var schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQueryType',
+        fields: {
+            hello: {
+                type: GraphQLString,
+                resolve() {
+                    return 'world';
+                }
+            }
+        }
+    })
+});
+
+
+
+module.exports = graphqlHTTP({
+    schema: schema,
+    graphiql: true
+});
